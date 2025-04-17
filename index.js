33f0defa4f9608749119ee02c47f32813d706c53
fida <fidzandroid@gmail.com>
2025-04-17 10:02:39 +0530

    cors

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 connectDB();
 
+
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
